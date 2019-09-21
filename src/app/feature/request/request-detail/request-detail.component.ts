6f3b24525990ea26d876f360138cf9c647a1d53f
lindmark421@gmail.com <lindmark421@gmail.com>
2019-09-21 18:40:47 -0400

    Request Detail

---
+++ src/app/feature/request/request-detail/request-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Request } from '@model/request.class';
+import { RequestService } from '@svc/request.service ';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-request-detail',
+  templateUrl: './request-detail.component.html',
+  styleUrls: ['./request-detail.component.css']
+})
+export class RequestDetailComponent implements OnInit {
+  request: Request = new Request ();
+  title: string = 'Request-Detail';
+
+  constructor(private requestSvc: RequestService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(parms => {
+      this.requestSvc.get(parms.id).subscribe(resp => {
+        this.request = resp as Request;
+        console.log('request detail'+this.request)
+      })
+    });
+  }
+  remove(){
+    this.requestSvc.delete(this.request.id).subscribe(resp => {
+      alert('Product'+this.request+'successfully deleted');
+      this.router.navigateByUrl('/product/list');
+
+    },
+    err => {
+      console.log('error deleting product');
+      console.log(err);
+    });
+  }
+
+}
